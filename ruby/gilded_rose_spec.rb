9da15330d1a83a186ef4052270473e382613af71
Murilo Azevedo <muriloazevedo338@gmail.com>
2024-03-04 20:58:49 -0300

    feat: update Gemfile and add specs

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -3,9 +3,72 @@ require 'rspec'
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+
+
+  context "Aged Brie" do
+    it "is good for sale" do
+      items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..1).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Aged Brie")
+      expect(result.first.sell_in).to be >= 0
+    end
+
+    it "expired" do
+      items = [Item.new(name="Aged Brie", sell_in=2, quality=0)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..4).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Aged Brie")
+      expect(result.first.sell_in).to be < 0
+      expect(result.first.quality).to be >= 4
+    end
+  end
+
+  context "Backstage passes to a TAFKAL80ETC concert" do
+    
+  end
+
+  context "Sulfuras, Hand of Ragnaros" do
+    it "should sell" do
+      items = [Item.new(name="Sulfuras, Hand of Ragnaros", sell_in=0, quality=80)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..4).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Sulfuras, Hand of Ragnaros")
+      expect(result.first.quality).to be >= 80
+    end
+  end
+
+  context "Conjured Mana Cake" do
+
+    it "expired" do
+      items = [Item.new(name="Conjured Mana Cake", sell_in=2, quality=0)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..4).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Conjured Mana Cake")
+      expect(result.first.sell_in).to be < 0
+    end
+
+    it "should sell" do
+      items = [Item.new(name="Conjured Mana Cake", sell_in=3, quality=6)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..2).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Conjured Mana Cake")
+      expect(result.first.sell_in).to be >= 0
+    end
   end
 end
