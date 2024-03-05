08333c938fe1d521d8acd89562947132e56fa2f6
Murilo Azevedo <muriloazevedo338@gmail.com>
2024-03-04 21:13:16 -0300

    test: add test to conjured

--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -31,7 +31,28 @@ describe GildedRose do
   end
 
   context "Backstage passes to a TAFKAL80ETC concert" do
-    
+    it "is good for sale" do
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=20)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..1).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Backstage passes to a TAFKAL80ETC concert",)
+      expect(result.first.sell_in).to be >= 0
+    end
+
+    it "expired" do
+      items = [Item.new(name="Backstage passes to a TAFKAL80ETC concert", sell_in=5, quality=49)]
+      gilded_rose = GildedRose.new items
+      result = nil
+      (0..6).each do |item|
+        result = gilded_rose.update_quality()
+      end
+      expect(result.first.name).to eq("Backstage passes to a TAFKAL80ETC concert")
+      expect(result.first.sell_in).to be < 0
+      expect(result.first.quality).to be >= 0
+    end
   end
 
   context "Sulfuras, Hand of Ragnaros" do
@@ -58,6 +79,7 @@ describe GildedRose do
       end
       expect(result.first.name).to eq("Conjured Mana Cake")
       expect(result.first.sell_in).to be < 0
+      expect(result.first.quality).to be 80
     end
 
     it "should sell" do
@@ -69,6 +91,7 @@ describe GildedRose do
       end
       expect(result.first.name).to eq("Conjured Mana Cake")
       expect(result.first.sell_in).to be >= 0
+      expect(result.first.quality).to be 80
     end
   end
 end
